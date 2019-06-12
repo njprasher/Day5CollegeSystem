6265a29bc0bb26c6fbb647663b71dc1b316d3857
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-12 19:30:58 -0400

    [Neeraj] Using an object and adding values to it then displaying the stored as well as computed values out of it

--- Day5CollegeSystem/main.swift
+++ Day5CollegeSystem/main.swift
@@ -17,3 +17,13 @@ p.lastName = "Prasher"
 
 print(p.id ?? "No Id", p.firstName ?? "No name", p.lastName!, p.fullName)
 
+s.firstName = "Neeraj"
+s.lastName = "Prasher"
+s.birthDate = Date()
+s.gender = Gender.MALE
+s.email = "prasher.neeraj99@gmail.com"
+s.marks = ["Math": 89,
+           "Science": 92,
+           "English": 95]
+
+print(s.firstName ?? "No first name", s.lastName ?? "No last name", s.birthDate ?? "No date of birth given", s.gender, s.email ?? "No email given", s.marks, s.total, s.percentage )
