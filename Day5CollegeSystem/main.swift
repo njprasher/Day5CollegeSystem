21cf02bb755719e9159448448ddc487831cba257
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-12 18:17:05 -0400

    Initial Commit

---
+++ Day5CollegeSystem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day5CollegeSystem
+//
+//  Created by Neeraj Prasher on 2019-06-12.
+//  Copyright © 2019 lambton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
